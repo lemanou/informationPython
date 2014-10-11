edc5ede0696aff71a03c2214b9a23646c099be5f
lemanou <lemanousakis@gmail.com>
2014-10-11 17:16:46 +0200

    Update sqlTutorial.py
    more testing

--- sqlTutorial.py
+++ sqlTutorial.py
@@ -5,7 +5,8 @@ from couchdb.mapping import Document, TextField, IntegerField, DateTimeField
 
 server = Server()
 db = Database('python-tests2 ')  # server.create('python-tests2')
-
+# db.name
+# del server['python-tests']
 
 # class Person(Document):
 #    name = TextField()
@@ -23,12 +24,26 @@ class myArticles(Document):
     added = DateTimeField(default=datetime.now)
 
 
-with open("dailyPosts/" + "myDictionary" + ".txt", "r") as myfile:
-    a = json.load(myfile)
+# with open("dailyPosts/" + "myDictionary" + ".txt", "r") as myfile:
+#    a = json.load(myfile)
 
 # print (a)
 
-article = myArticles(id='1', myJson=a)
-article.store(db)
+# article = myArticles(id='1', myJson=a)
+# article.store(db)
+# print(article.myJson)
 
-print(article.myJson)
+# json_string = json.dumps(a, sort_keys=True, indent=2)
+# print json_string
+
+
+# article = myArticles(myJson=a)
+# article.store(db)
+# doc = {'foo': 'bar', '_id':'1'}
+# db.save(a)
+
+article = myArticles.load(db, article.id)
+comment = article.myJson[0]
+
+print(comment)
+
