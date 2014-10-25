218df5e5a1670ccb123638b3fe5d6260c05f96bc
lemanou <lemanousakis@gmail.com>
2014-10-25 16:45:51 +0200

    Reading Body and Comments now

--- readPage.py
+++ readPage.py
@@ -7,32 +7,93 @@ Information.dk mining
 """
 
 import urllib
-import json
+# import json
 from bs4 import BeautifulSoup
 import jsonToCouchDB
+
+
+def removeParenthesis(tmp):
+    if '(' in tmp:
+        tmp = tmp.replace("(", "")
+    if ')' in tmp:
+        tmp = tmp.replace(")", "")
+    return tmp
+
+
+# Parsing the articles based on categories and tags
+def getBody(tree, postID):
+    body = ""
+    neededClass = ""
+
+    if 'nyhedsblog' in postID:
+        neededClass = "field field-name-field-underrubrik"
+    else:
+        neededClass = "field field-name-body"
+
+    for text in tree.findAll("div", class_=neededClass):
+        for childdiv in text.find_all('p'):
+            if not (childdiv.string is None):
+                body += childdiv.string
+
+    return body
+
+
+def getComments(tree):
+    article = {}
+    dAuthor = {}
+    dDomment = {}
+    for comments in tree.findAll(class_="comment-wrapper"):
+        myArray = []
+        myArray2 = []
+        if not (comments is None):
+            count = 0
+            for author in comments.findAll("div", class_="comment-name"):
+                for childdiv in author.find_all('a'):
+                    if not (childdiv.string is None):
+                        count += 1
+                        myArray.append(childdiv.string)
+            count = 0
+            neededClass = "field-item even"
+            for commentBody in comments.findAll("div", class_=neededClass):
+                count += 1
+                cmnt = ""
+                for childdiv in commentBody.find_all('p'):
+                    if not (childdiv.string is None):
+                        cmnt += childdiv.string
+                myArray2.append(cmnt)
+        for id in range(len(myArray)):
+            dAuthor[id] = myArray[id]
+            dDomment[id] = myArray2[id]
+        article["commentsAuthor"] = dAuthor
+        article["commentsBody"] = dDomment
+    return article
 
 
 def parseNormal(myLink, postID):
     article = {}
     # Open the site
     url = myLink + postID
-    # print "Opening: " + url
     htmlfile = urllib.urlopen(url)
     htmltext = htmlfile.read()
     tree = BeautifulSoup(htmltext)
+
     # Get the relevant parts from the page
     for title in tree.findAll("h1", class_="node-title"):
-        # print title
         article["title"] = title.contents[0]
+
     neededClass = "field field-name-field-underrubrik"
     for abstract in tree.findAll("div", class_=neededClass):
-        # print abstract
         article["abstract"] = abstract.contents[0]
-        neededClass = "field-name-comment-count-link-top"
+
+    neededClass = "field-name-comment-count-link-top"
     for numCom in tree.findAll("div", class_=neededClass):
-        # print str(numCom)[104:107]
-        article["comments"] = numCom.contents[0].contents[0]
-    article["id"] = postID
+        tmp = ""
+        tmp = numCom.contents[0].contents[0][13:15]
+        article["numberOfComments"] = removeParenthesis(tmp)
+
+    article["body"] = getBody(tree, postID)
+    article["comments"] = getComments(tree)
+
     return article
 
 
@@ -40,22 +101,29 @@ def parseTelegram(myLink, postID):
     article = {}
     # Open the site
     url = myLink + postID
-    # print "Opening: " + url
     htmlfile = urllib.urlopen(url)
     htmltext = htmlfile.read()
     tree = BeautifulSoup(htmltext)
+
     # Get the relevant parts from the page
     for title in tree.findAll("h1", class_="node-title"):
-        # print title
         article["title"] = title.contents[0]
+
     neededClass = "field field-name-field-ritzau-subheader"
     for abstract in tree.findAll("div", class_=neededClass):
-        # print abstract
         article["abstract"] = abstract.contents[0]
-    for numCom in tree.findAll("a", class_="active"):
-        # print str(numCom)[104:107]
-        article["comments"] = numCom.contents[0]
-    article["id"] = postID
+
+    if 'protokol' in postID:
+        article["numberOfComments"] = "unknown"
+    else:
+        for numCom in tree.findAll("a", class_="active"):
+            tmp = ""
+            tmp = numCom.contents[0][13:15]
+            article["numberOfComments"] = removeParenthesis(tmp)
+
+    article["body"] = getBody(tree, postID)
+    article["comments"] = getComments(tree)
+
     return article
 
 
@@ -63,22 +131,26 @@ def parseNyheder(myLink, postID):
     article = {}
     # Open the site
     url = myLink + postID
-    # print "Opening: " + url
     htmlfile = urllib.urlopen(url)
     htmltext = htmlfile.read()
     tree = BeautifulSoup(htmltext)
+
     # Get the relevant parts from the page
     for title in tree.findAll("div", class_="field field-name-title"):
-        # print title.contents[0].contents[0]
         article["title"] = title.contents[0].contents[0]
+
     neededClass = "field field-name-field-underrubrik"
     for abstract in tree.findAll("div", class_=neededClass):
-        # print abstract
         article["abstract"] = abstract.contents[0]
+
     for numCom in tree.findAll("li", class_="comment last"):
-        # print numCom.contents[0].contents[0]
-        article["comments"] = numCom.contents[0].contents[0]
-    article["id"] = postID
+        tmp = ""
+        tmp = numCom.contents[0].contents[0][13:15]
+        article["numberOfComments"] = removeParenthesis(tmp)
+
+    article["body"] = getBody(tree, postID)
+    article["comments"] = getComments(tree)
+
     return article
 
 
@@ -90,20 +162,26 @@ def parseBlog(myLink, postID):
     htmlfile = urllib.urlopen(url)
     htmltext = htmlfile.read()
     tree = BeautifulSoup(htmltext)
+
     # Get the relevant parts from the page
     for title in tree.findAll("h1", class_="node-title"):
-        # print title.contents[0]
         article["title"] = title.contents[0]
+
     neededClass = "field field-name-body"
     for div in tree.find_all(class_=neededClass):
         # Get the first child (<p>) in the div
         for childdiv in div.find_all('p'):
             article["abstract"] = childdiv.string
             break
+
     for numCom in tree.findAll("li", class_="comment last"):
-        # print numCom.contents[0].contents[0]
-        article["comments"] = numCom.contents[0].contents[0]
-    article["id"] = postID
+        tmp = ""
+        tmp = numCom.contents[0].contents[0][13:15]
+        article["numberOfComments"] = removeParenthesis(tmp)
+
+    article["body"] = getBody(tree, postID)
+    article["comments"] = getComments(tree)
+
     return article
 
 
@@ -111,28 +189,34 @@ def parseFilme(myLink, postID):
     article = {}
     # Open the site
     url = myLink + postID
-    # print "Opening: " + url
     htmlfile = urllib.urlopen(url)
     htmltext = htmlfile.read()
     tree = BeautifulSoup(htmltext)
+
     # Get the relevant parts from the page
     for title in tree.findAll("div", class_="field field-name-title"):
         # print title.contents[0].contents[0]
         article["title"] = title.contents[0].contents[0]
+
     neededClass = "field field-name-body"
     for div in tree.find_all(class_=neededClass):
         # Get the first child (<p>) in the div
         for childdiv in div.find_all('p'):
             article["abstract"] = childdiv.string
             break
+
     for numCom in tree.findAll("li", class_="comment last"):
-        # print numCom.contents[0].contents[0]
-        article["comments"] = numCom.contents[0].contents[0]
-    article["id"] = postID
+        tmp = ""
+        tmp = numCom.contents[0].contents[0][13:15]
+        article["numberOfComments"] = removeParenthesis(tmp)
+
+    article["body"] = getBody(tree, postID)
+    article["comments"] = getComments(tree)
+
     return article
 
 
-def parseHTML():
+def parseHTML(myDB):
 
     # Read and split the links from a file
     newsFile = open("myNewsSites.txt")
@@ -179,6 +263,7 @@ def parseHTML():
             check = True
         elif (urlLength == 41):
             # print "Telegram OR Protocol: " + str(urlLength)
+            # Protocols have no abstract or numCom
             myArticle = parseTelegram(myLink, postID)
             myDict["article"] = myArticle
             check = True
@@ -203,18 +288,16 @@ def parseHTML():
             print "\t NOT PROCESSED -  Unknown length: " + str(urlLength)
 
         if (check):
-            with open("dailyPosts" + postID + ".txt", "w+") as myfile:
-                myResult = json.dump(myDict, myfile, indent=4)
-            jsonToCouchDB.storeInDB("dailyPosts/" + postID + ".txt")
-    # for x in myDict:
-    #   print (x)
-    #   for y in myDict[x]:
-    #       print (y,':',myDict[x][y])
+            # with open("dailyPosts" + postID + ".txt", "w+") as myfile:
+            #     json.dump(myDict, myfile, indent=4)
+            # jsonToCouchDB.storeFileInDB("dailyPosts/" + postID + ".txt")
+            jsonToCouchDB.storeDicInDB(myDB, myDict, postID)
 
 
 def main():
-    parseHTML()
-    jsonToCouchDB.loadFromDB()
+    jsonToCouchDB.createDB('information_dk_articles')
+    parseHTML('information_dk_articles')
+    # jsonToCouchDB.loadFromDB()
 
 
 if __name__ == "__main__":
