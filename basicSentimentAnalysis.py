e99fa8b9ac9ef575d6faa5eb480e31fd4c9c9b61
Eleftherios Manousakis <lemanousakis@gmail.com>
2014-11-03 16:29:55 +0100

    Updated basicSentiment

--- basicSentimentAnalysis.py
+++ basicSentimentAnalysis.py
@@ -5,50 +5,59 @@ Created on Sat Nov 1 19:39:42 2014
 Information.dk mining
 ---------------------------------------------------------
 """
-from nltk import sent_tokenize, word_tokenize
-import json
-import re  # Use regular expressions to find the numbers in the sentiment list
-    
- 
-def splitS(text):
-    sents = sent_tokenize(text)
-    return sents
+from nltk import word_tokenize
+import couchdb
 
 
-def splitW(sentence):
-    tokens = word_tokenize(sentence) 
+def splitTextToWords(text):
+    tokens = word_tokenize(text) 
     return tokens
 
 
-def word_score(wordCheck, acum_score):
-    #Load Sentiment List and check for word
+def createSentDict():
+    sentList = {}
     with open('Nielsen2011Sentiment_afinndk.txt', 'r') as f:
-        for line in f:            
-            if wordCheck in line.split():
-                print re.findall('\d+', line)[0]
-                return int(re.findall('\d+', line)[0])
-            else:                
-                print "WORD: " + wordCheck.encode('utf-8','replase') + " NOT FOUND."
-                return acum_score
+        for line in f:
+           (key, val) = line.split()
+           sentList[key] = int(val) 
+    return sentList
 
 
-def sentiment_score(wordsList):
-    return sum([word_score(word, 0.0) for word in wordsList])
-
+def getSentiment(article):
+    # Create Dictionary from sentiment file
+    sentList =  createSentDict()
+    
+    # Get the body of the article, split it to words and lowercase them
+    articleBody = (article['article']['body'])
+    wordsList = splitTextToWords(articleBody)
+    wordsList = [x.lower() for x in wordsList]
+    
+    # Calculate the sentiment score
+    sum = 0
+    count = 0
+    avg = 0
+    for word in wordsList:
+        if word in sentList:
+            count+=1
+            sum+=sentList[word]
+    if count != 0:
+        avg = round (float(sum)/count, 2)
+    myDict = {'ArticleID': article['_id'], 'Sentiment': sum, 'NumberOfWords':count, 'Average':avg}
+    return myDict
+    
  
 def main():
-    json_data=open('dailyPosts/514201.txt', 'r')
-    data = json.load(json_data)
-    text = (data['article']['body'])
-    #print text.encode('utf-8','replase')
-     
-    splitted_sentences = splitS(text)
-    
-    #print splitted_sentences
-    words = splitW(text)
-     
-    #print words
-    print "Sent: " + str(sentiment_score(words))
+    # json_data=open('dailyPosts/514321.txt', 'r')
+    couch = couchdb.Server()
+    db = couch['information_dk_articles']
+    myList = []
+    for article in db:
+        myList.append(getSentiment(dict(db.get(article))))
+    #print myList
+    f = open('ResultSentiment.txt','w')
+    for item in myList:
+        f.write("%s\n" % item)
+    f.close()
 
 if __name__ == "__main__":
     main()
