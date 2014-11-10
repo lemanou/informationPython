0745304f4055ccf4e21ec3a09177f378b8e8ba06
Sigurd Knarhøi Johannsen <sigurd_johannsen@hotmail.com>
2014-11-10 14:58:58 +0100

    sorted sentiment graph

--- plottingGettingDataFromFile.py
+++ plottingGettingDataFromFile.py
@@ -3,8 +3,9 @@
 Created on Sun Sep 21 10:31:23 2014
 
 @author: Sigurd
-"""
 
+"""
+import collections
 
 with open( "ResultSentiment.txt", "r") as FileToRead:
     FileToWrite = open('DataToPlot.txt', 'w')
@@ -17,62 +18,21 @@ with open( "ResultSentiment.txt", "r") as FileToRead:
         D[Average] = val3.strip(': ,')
         D[Sentiment] = val4.strip(': ,}')
         # CHANGE HERE
-        Data.append(D[Average])
+        DintAverage = float(D[Average])
+        Data.append(DintAverage)
 
-print Data
-FileToWrite.write("%s\n" % Data)
+sortedList = sorted(Data)
+print sortedList
+
+FileToWrite.write("%s\n" % sortedList)
 FileToWrite.close()
 
 import matplotlib.pyplot as plt
 
 plt.figure(1)
-plt.plot(Data)
+plt.plot(sortedList)
 plt.xlabel("Articles")
 plt.ylabel("Average Sentiment")
-plt.title("Average Sentiment of Articles")
-plt.savefig("SentimentAverageCurve.png")
+plt.title("Average Sentiment For Each Article")
+plt.savefig("SentimentSortedAverageCurve.png")
 plt.show()
-
-
-
-
-
-
-"""
-FileToWrite = open('DataToPlot.txt', 'w')
-for item in myList:
-    FileToWrite.write("%s\n" % item)
-FileToWrite.close()
-
-
-import collections
-
-dict = {'a': 1, 'c': 2, 'b': 3, 'd': 4}
-
-sortedDict = collections.OrderedDict(sorted(dict.items()))
-print sortedDict
-
-print ("lineData: ")
-print lineDataArray
-
-
-
-print ("Keys:")
-for k in sortedDict.keys() :
-    for value in dict:
-        " if dict[ value ] == k:"
-        print k
-        break   
-print ("Values:")
-for v in sortedDict.values() :
-    for key in dict:
-        "if dict[ key ] == v:"
-        print v
-        break
-print ("Items:")
-for v in sortedDict.values() :
-    for key in dict:
-        if dict[ key ] == v:
-            print key, v
-            break
-"""
